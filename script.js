57570e7842bbb6df9ba15589f2639235dae1d651
mohammedash-iq <mohammedashiq206@gmail.com>
2025-06-24 12:49:02 +0530

    added a bit of logic into the js file and also added some  temp images for preview

--- script.js
+++ script.js
@@ -52,30 +52,50 @@ let catogory = [
     image: "catogory.png",
   },
 ];
-const searchbtn = document.getElementById("searchbtn");
+
 const searchInput = document.getElementById("searchinput");
 const catagoryCardContainer = document.getElementById("catagoryCardContainer");
 const mainCardContainer = document.getElementById("mainCardContainer");
+loadCatogoryCards();
 function searchRecipe() {
-  const value = searchInput.value;
   loadCards();
 }
 function loadCards() {
+  searchData();
   recipies.map((recipe) => {
-    const card = document.createElement();
-    const image = document.createElement();
-    const title = document.createElement();
-    const disc = document.createElement();
-    const likebutton = document.createElement();
-    mainCardContainer.appendChild(card, image, title, disc, likebutton);
+    const card = document.createElement("div");
+    const image = document.createElement("img");
+    image.setAttribute("src", recipe.image);
+    const title = document.createElement("h4");
+    title.innerText = recipe.name;
+    const disc = document.createElement("p");
+    disc.innerText = recipe.description;
+    const likebutton = document.createElement("button");
+    likebutton.innerText = "❤";
+    card.appendChild(image);
+    card.appendChild(title);
+    card.appendChild(disc);
+    card.appendChild(likebutton);
+    mainCardContainer.appendChild(card);
+    mainCardContainer.style.display = "flex";
   });
 }
 function loadCatogoryCards() {
-  recipies.map((recipe) => {
-    const card = document.createElement();
-    const image = document.createElement();
-    const title = document.createElement();
-    const disc = document.createElement();
-    catagoryCardContainer.appendChild(card, image, title, disc, likebutton);
+  catogory.map((recipe) => {
+    const card = document.createElement("div");
+    const image = document.createElement("img");
+    image.setAttribute("src", recipe.image);
+    const title = document.createElement("h5");
+    title.innerText = recipe.name;
+    const disc = document.createElement("p");
+    disc.innerText = recipe.disc;
+    card.appendChild(title);
+    card.appendChild(image);
+    card.append(disc);
+    catagoryCardContainer.appendChild(card);
+    catagoryCardContainer.style.display = "flex";
   });
+}
+function searchData() {
+  console.log("fetching api for the required data/////");
 }
