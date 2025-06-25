34219eb9f8f32c00c14b14c39f7048d3873fb58e
mohammedash-iq <mohammedashiq206@gmail.com>
2025-06-25 12:53:00 +0530

    fixed small things like adding youtube links and origin of dish in the recipe card and also tried fixing the style part ....again not my thing

--- script.js
+++ script.js
@@ -150,15 +150,29 @@ function updateRecipeCard(event) {
 function loadRecipe(data) {
   recipeContainer.innerHTML = "";
   const card = document.createElement("div");
-  card.className = "bg-emerald-100 m-1 flex rounded-md ";
   const image = document.createElement("img");
   image.setAttribute("src", data.strMealThumb);
-  image.className = "flex-2/4 w-0.5 p-2 rounded-2xl ";
+  image.className = "m-6 rounded-2xl w-[30%] max-sm:w-[40%]";
   const title = document.createElement("h5");
-  title.className =
-    "text-center font-semibold text-2xl p-2  text-emerald-400 p-2 flex-2/4 ";
+  title.className = "text-left font-semibold text-2xl m-6 text-emerald-400";
   title.innerText = data.strMeal;
+  const details = document.createElement("p");
+  details.innerHTML = `Category:  ${data.strCategory}<br><br>Origin: ${data.strArea}`;
+  details.className = "m-6 ";
+  const instruction = document.createElement("p");
+  instruction.innerHTML = `Instructions:<br> ${data.strInstructions}`;
+  instruction.className = "m-6 bg-emerald-200";
+  const youtubelink = document.createElement("a");
+  youtubelink.innerHTML = `Video: Click here to watch a youtube tutorial!`;
+  youtubelink.setAttribute("href", data.strYoutube);
+  youtubelink.className = "m-6";
+  const container = document.createElement("div");
+  container.className = "flex justify-between bg-emerald-200 rounded-xl";
   card.appendChild(title);
-  card.appendChild(image);
-  recipeContainer.appendChild(card);
+  card.appendChild(details);
+  card.appendChild(youtubelink);
+  container.appendChild(card);
+  container.appendChild(image);
+  recipeContainer.appendChild(container);
+  recipeContainer.appendChild(instruction);
 }
